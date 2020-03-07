cf650d80d28f61c3f9991cb0624776b79839f4d5
Nathan Virayie <nathan.virayie@icloud.com>
2020-03-07 01:33:03 +0100

    ajout d'une classe de parametre du qlearning

---
+++ IA/src/data/QLearningPara.java
@@ -0,0 +1,13 @@
+package data;
+
+public class QLearningPara {
+	public static final int DIM_MAP =25;
+	
+	public static double GAMMA = 0.9; // exploration rate , détermine l'importance des futures récompenses , facteur 0 l'agent ne considéra que les récompenses actuelles, un facteur approchant 1 il visera une récompense élevée à long terme 
+	
+	public static double ALPHA = 0.2; // learning rate : facteur 0 empêchera l'agent d'apprendre, facteur de 1 ne  considérerait que les informations les plus récentes
+
+	public static final int MALUS = -500;
+	
+	public static final int REWARD = 100;
+}
