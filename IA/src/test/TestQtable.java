79c6315eec19e60954ca9f535250bbf4616f6485
Nathan Virayie <nathan.virayie@icloud.com>
2020-03-21 18:32:02 +0100

    correction d'erreur d'appel

--- IA/src/test/TestQtable.java
+++ IA/src/test/TestQtable.java
@@ -5,10 +5,10 @@ public class TestQtable {
 
 	public static void main(String[] args) {
 		QTable q = new QTable(); 
-		q.afficher();
+		q.print();
 		QFonction f=new QFonction(q,0.9,0.8);
 		f.update(0,0,1,0,100);
-		q.afficher();
+		q.print();
 	}
 
 }
