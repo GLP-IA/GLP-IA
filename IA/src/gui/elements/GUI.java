01593713db2d56e6510d3623e1d447414c4468d6
mhk21 <mhk21@DESKTOP-JHU4M3F>
2020-03-25 10:47:56 +0100

    commit

--- IA/src/gui/elements/GUI.java
+++ IA/src/gui/elements/GUI.java
@@ -107,7 +107,12 @@ public class GUI extends JFrame implements Runnable{
 		map.initMapQLearning(t);//initialise la carte
 		coreQ= new QLearningCore(map,t,character);
 		
+		
+		
 		//instrumentation : exploration (on) et exploitation (off)
+			
+		
+		
 		JLabel label_instrumQlearning = new JLabel("");
 		label_instrumQlearning.setBounds(1000, 420, 500, 500);
 		this.getContentPane().add(label_instrumQlearning);
